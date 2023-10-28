1d5f31b3a33f353689e428f1cba815a1511f1b36
Pedro Matoso <pedroomatoso@gmail.com>
2023-10-28 15:58:47 -0300

    add an alert dialog before deleting issue

--- src/app/issues/[id]/DeleteIssueButton.tsx
+++ src/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -16,9 +16,34 @@ export default function DeleteIssueButton({ issueId }: { issueId: string }) {
   }
 
   return (
-    <Button color="red" onClick={handleDeleteIssue}>
-      <HiOutlineTrash />
-      Delete
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <HiOutlineTrash />
+          Delete
+        </Button>
+      </AlertDialog.Trigger>
+
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
+        <AlertDialog.Description size="3">
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red" onClick={handleDeleteIssue}>
+              Delete issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 }
