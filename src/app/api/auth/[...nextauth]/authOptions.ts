d64b4c7599942a9283b8b6714d87d1e87d251208
Pedro Matoso <pedroomatoso@gmail.com>
2023-10-28 22:46:55 -0300

    implement login using next-auth and google provider

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,17 @@
+import prisma from '@/prisma/client'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: { 
+    strategy: 'jwt'
+  },
+}
